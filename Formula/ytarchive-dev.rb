6e314fc3feb922a0b50d04c2ee1fa5e40ede0744
omerien <53191414+omerien@users.noreply.github.com>
2023-12-31 00:32:16 +0100

    Update ytarchive-dev.rb

--- Formula/ytarchive-dev.rb
+++ Formula/ytarchive-dev.rb
@@ -3,7 +3,7 @@ class YtarchiveDev < Formula
   homepage 'https://github.com/Kethsar/ytarchive'
   url 'https://github.com/Kethsar/ytarchive/archive/refs/tags/latest.zip'
   version '5d91faa'
-  sha256 '56da94fe0fec98e9f6324c56d8b8475e9035cf93931f511361cefa6f795a8375'
+  sha256 'aaaa94fe0fec98e9f6324c56d8b8475e9035cf93931f511361cefa6f795a8375' 
   license 'MIT'
   head 'https://github.com/Kethsar/ytarchive.git', branch: 'dev'
   conflicts_with "omerien/ytarchive/ytarchive", because: "both have a ytarchive binary"
