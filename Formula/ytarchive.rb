6db1778644ebcbff5a646237e32ba2b044fb1d52
omerien <53191414+omerien@users.noreply.github.com>
2024-09-27 16:00:40 +0200

    0.5.0

--- Formula/ytarchive.rb
+++ Formula/ytarchive.rb
@@ -3,7 +3,7 @@ class Ytarchive < Formula
   homepage 'https://github.com/Kethsar/ytarchive'
   url 'https://github.com/Kethsar/ytarchive/archive/refs/tags/v0.5.0.zip'
   version '0.5.0'
-  sha256 'c39a9dd2afff81c7ef76ffbb85c38a4a47a0fdaf520cfd72598336eed7089f47'
+  sha256 '7e80b0e51e3b7e7c692bdc86fb5a2f2a14aa67e9cbd0a821ee5049bfc5e4a100'
   license 'MIT'
   head 'https://github.com/Kethsar/ytarchive.git', branch: 'dev'
 
