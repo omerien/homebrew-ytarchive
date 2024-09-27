827f8974e6baa7d5abd04dc5c31b3f62a493ae05
omerien <53191414+omerien@users.noreply.github.com>
2024-09-27 16:19:20 +0200

    Update ytarchive-dev.rb

--- Formula/ytarchive-dev.rb
+++ Formula/ytarchive-dev.rb
@@ -2,7 +2,7 @@ class YtarchiveDev < Formula
   desc 'Archive live and upcoming Youtube.com live streams'
   homepage 'https://github.com/Kethsar/ytarchive'
   url 'https://codeload.github.com/Kethsar/ytarchive/zip/refs/heads/dev'
-  version '3c425e5'
+  version 'fb5b172'
   sha256 '237c8bc4ce85218ec31083504115b61216dc802d30406762df6c01a965b7eb20' 
   license 'MIT'
   head 'https://github.com/Kethsar/ytarchive.git', branch: 'dev'
